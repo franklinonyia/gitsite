f004aa8f93dc2eeef8090a68945703073f397190
franklinonyia <bukkyonyia@gmail.com>
2016-11-15 13:03:14 +0100

    please take note

---
+++ images/form.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>form</title>
+</head>
+
+<body style="background-color:dedede;">
+	<span>username:<input type="text" name="username"placeholder="username" ></input></span><p>
+</body>
+</html>
